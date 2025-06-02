d32b7f1a74fce4c345eac98d0f4b9d8e6d277e08
Katerina <e.mayakovskaya@gmail.com>
2025-06-02 20:02:45 +0300

    Remove sensitive data from settings.py

--- settings.py
+++ settings.py
@@ -7,9 +7,9 @@ load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
     }
